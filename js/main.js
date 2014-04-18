c3b4307390603ba2b93baf0b17290620e4add152
Miguel Roman <miguelerm@gmail.com>
2014-04-17 20:47:41 -0600

    se corrigio un error con disqus
    Cuando no se mostraban comentarios en una pagina daba un error disqus

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // Disqus.com script
-(function(window, document, disqus_shortname, remote_hostname) {
+(function(window, document, disqus_shortname, remote_hostname, disqus_enabled) {
     if (window.location.hostname !== remote_hostname)
         return;
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})(window, document, disqus_shortname, remote_hostname);
+
+    if (disqus_enabled) {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    }
+})(window, document, disqus_shortname, remote_hostname, disqus_enabled);
